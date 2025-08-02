b78f7a34f949245eb20978916c90b6e20eb4e365
jxeong <jxeong@gmail.com>
2025-08-02 17:23:15 +0900

    Intro Logo 수정

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,14 +3,13 @@ import './Intro.css';
 import logo from './exampleLogo2.png';
 
 // 배경 영상들 import
-import bg3 from './background3.mp4';
 import bg4 from './background4.mp4';
 import bg5 from './background5.mp4';
 import bg6 from './background6.mp4';
 
 const Intro = ({ onClickLogo, fadeOut }) => {
   // 영상 배열에서 1개 랜덤 선택
-  const videoSources = useMemo(() => [bg3, bg4, bg5, bg6], []);
+  const videoSources = useMemo(() => [bg4, bg5, bg6], []);
   const [selectedVideo] = useState(() => {
     const randomIndex = Math.floor(Math.random() * videoSources.length);
     return videoSources[randomIndex];
@@ -20,7 +19,7 @@ const Intro = ({ onClickLogo, fadeOut }) => {
   useEffect(() => {
     const timer = setTimeout(() => {
       onClickLogo();
-    }, 5000);
+    }, 7000);
     return () => clearTimeout(timer);
   }, [onClickLogo]);
 
@@ -36,7 +35,7 @@ const Intro = ({ onClickLogo, fadeOut }) => {
       <div className="intro-content">
         <img src={logo} alt="로고" className="intro-logo" />
         <h1 className="intro-title">2025<br/>DSWU SOFTWARE<br />GRADUATION EXHIBITION</h1>
-        <h3>Click to Enter!</h3>
+        <h3 className="intro-title2">Click to Enter!</h3>
       </div>
     </div>
   );
